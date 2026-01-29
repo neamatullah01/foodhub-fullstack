9c27514156df9eed9e30f8274ba8a0415f01fe67
neamatullah01 <neamat22205101908@diu.edu.bd>
2026-01-29 20:57:23 +0600

    feat: add create meal

--- backend/src/modules/meal/meal.controller.ts
+++ backend/src/modules/meal/meal.controller.ts
@@ -3,7 +3,8 @@ import { mealService } from "./meal.service";
 
 const addMeal = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await mealService.addMeal(req.body);
+    const userId = req.user?.id;
+    const result = await mealService.addMeal(userId as string, req.body);
     res.status(201).json(result);
   } catch (e) {
     next(e);
--- backend/src/modules/meal/meal.service.ts
+++ backend/src/modules/meal/meal.service.ts
@@ -1,9 +1,24 @@
 import { prisma } from "../../lib/prisma";
 import { AddMealInput } from "./meal.types";
 
-const addMeal = async (data: AddMealInput) => {
+const addMeal = async (userId: string, data: AddMealInput) => {
+  const provider = await prisma.providerProfile.findUnique({
+    where: { userId },
+  });
+
+  if (!provider) {
+    throw new Error("Provider profile not found!");
+  }
+
+  if (!provider.isApproved) {
+    throw new Error("Provider not approved!");
+  }
+
   return await prisma.meal.create({
-    data,
+    data: {
+      ...data,
+      providerId: provider.id,
+    },
   });
 };
 
