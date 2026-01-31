a28b795f7d6468f43c81ba19cdae17d983498094
neamatullah01 <neamat22205101908@diu.edu.bd>
2026-01-31 18:33:04 +0600

    feat(auth): add seed admin script

---
+++ backend/src/scripts/seedAdmin.ts
@@ -0,0 +1,50 @@
+import { prisma } from "../lib/prisma";
+import { Role } from "../middlewares/verifyAuth";
+
+async function seedAdmin() {
+  try {
+    const adminData = {
+      name: "FoodHub Admin",
+      email: "admin@gmail.com",
+      role: Role.ADMIN,
+      password: "admin1234",
+    };
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: adminData.email,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("Admin already exists!!");
+    }
+
+    const signUpAdmin = await fetch(
+      "http://localhost:5000/api/auth/sign-up/email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Origin: "http://localhost:5000",
+        },
+        body: JSON.stringify(adminData),
+      },
+    );
+
+    if (signUpAdmin.ok) {
+      await prisma.user.update({
+        where: {
+          email: adminData.email,
+        },
+        data: {
+          emailVerified: true,
+        },
+      });
+    }
+    console.log("******* SUCCESS ******");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+seedAdmin();
